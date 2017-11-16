20e89b22ef40c0e427cefa0557a76e64091e8d57
muslu <admin@makdos.com>
2017-11-16 13:38:24 +0300

    Uygulama isimleri eklendi

--- blog/apps.py
+++ blog/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
-
+from django.utils.translation import ugettext_lazy as _
 
 class BlogConfig(AppConfig):
-    name = 'blog'
+    name            =   'blog'
+    verbose_name    =   _('Blog')
