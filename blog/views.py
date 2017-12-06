2f850911fb97a48b419a90f814b32aa299f704ed
Muslu Y <admin@makdos.com>
2017-12-06 15:39:05 +0300

    gitle silindi. MErhaba düzeltildi.

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,4 @@ from django.http import HttpResponse
 
 
 def anasayfa(request):
-    return HttpResponse("MErhaba")
-
-
-def gitle(request):
-    import subprocess
-    process = subprocess.Popen(["cd /home/muslu/django/pythontc;", "git pull origin master"], stdout=subprocess.PIPE)
-    output = process.communicate()[0]
-    return HttpResponse(output)
+    return HttpResponse("Merhaba")
